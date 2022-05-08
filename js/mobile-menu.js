599a0853bed1403b3ba04d6057b0d3f0354d3a8c
vovanfix <proskyrinv.g@gmail.com>
2022-05-08 19:40:35 +0300

    footer-end

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,6 +4,7 @@
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
     menuBtnRef.classList.toggle('is-open');
+    document.body.classList.toggle('menu-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
   });
